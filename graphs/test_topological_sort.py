911a8297abfb40fcae540f05646eeb4e8fddb221
Yehuda Sapir <yehuda.sapir@Yehudas-MacBook-Pro.local>
2017-02-10 15:45:51 +0200

    start no cycle varification - missing tests

--- graphs/test_topological_sort.py
+++ graphs/test_topological_sort.py
@@ -3,12 +3,65 @@ import topological_sort as ts
 
 class TestVertex:
 	def test_add_adjecency_good(self):
-		vertex = ts.Vertex(name = 'A',value = 0)
-		vertex.add_adjecency('B')
-		assert vertex.adjacency_list.head.cargo == 'B'
+		vertex1 = ts.Vertex(name = 'A',value = 0)
+		vertex2 = ts.Vertex(name = 'B',value = 0)
+		vertex1.add_adjecency(vertex2)
+		assert vertex1.adjacency_list.head.cargo.name == 'B'
 
 	def test_add_adjecency_error(self):
-		vertex = ts.Vertex(name = 'A',value = 0)
-		vertex.add_adjecency('B')
+		vertex1 = ts.Vertex(name = 'A',value = 0)
+		vertex2 = ts.Vertex(name = 'B',value = 0)
+		vertex1.add_adjecency(vertex2)
 		with pytest.raises(ValueError):
-			vertex.add_adjecency('B')
+			vertex1.add_adjecency(vertex2)
+
+	def test_add_adjecency_in_degree(self):
+		vertex1 = ts.Vertex(name = 'A',value = 0)
+		vertex2 = ts.Vertex(name = 'B',value = 0)
+		vertex1.add_adjecency(vertex2)
+		assert vertex2.in_degree == 1
+		vertex3 = ts.Vertex(name = 'C',value = 0)
+		vertex3.add_adjecency(vertex2)
+		assert vertex2.in_degree == 2
+
+
+class TestDirectedGraph:
+	def test_add_vertex_good(self):
+		dag = ts.DirectedGraph()
+		dag.add_vertex(name = 'A')
+		assert dag.vertices[0].name == 'A'
+		dag.add_vertex(name = 'B')
+		assert dag.vertices[1].name == 'B'
+
+	def test_add_vertex_bad(self):
+		dag = ts.DirectedGraph()
+		dag.add_vertex(name = 'A')
+		with pytest.raises(ValueError):
+			dag.add_vertex(name = 'A')
+
+	def test_add_adge_good(self):
+		dag = ts.DirectedGraph()
+		dag.add_vertex(name = 'A')
+		dag.add_vertex(name = 'B')
+		dag.add_adge('A','B')
+		assert dag.vertices[0].adjacency_list.head.cargo.name == 'B'
+
+	def test_add_adge_no_such_adge(self):
+		dag = ts.DirectedGraph()
+		with pytest.raises(ValueError):
+			dag.add_adge('A','B')
+		dag.add_vertex(name = 'A')
+		with pytest.raises(ValueError):
+			dag.add_adge('A','B')
+		with pytest.raises(ValueError):
+			dag.add_adge('A','A')
+		with pytest.raises(ValueError):
+			dag.add_adge('B','B')
+
+	def test_find_vertex_good(self):
+		dag = ts.DirectedGraph()
+		dag.add_vertex(name = 'A')
+		assert dag.find_vertex('A').name == 'A'
+		
+
+		
--- graphs/topological_sort.py
+++ graphs/topological_sort.py
@@ -10,14 +10,22 @@ class Vertex:
 		self.value = value
 		self.adjacency_list = ll.LinkedList()
 		self.in_degree = 0
+		self.parents = Set()
 
-	def add_adjecency(self,name_target):
+	def add_adjecency(self,vertex):
 		adge = self.adjacency_list.head
 		while (adge):
-			if adge.cargo == name_target:
-				raise ValueError
+			if adge.cargo == vertex:
+				raise ValueError("adge already exists for:" + vertex.name)
 			adge = adge.next
-		self.adjacency_list.add(ll.Node(name_target))
+		self.adjacency_list.add(ll.Node(vertex))
+		vertex.in_degree+=1
+
+	def __eq__(self,other):
+		return self.name == other.name
+
+	def __ne__(self,other):
+		return self.name <> other.name
 
 
 class DirectedGraph:
@@ -30,15 +38,39 @@ class DirectedGraph:
 		if name not in self.names_set:
 			node = Vertex(name,value,adjacency_list)
 			self.vertices.append(node)
+			self.names_set.add(name)
 		else:
-			raise ValueError
+			raise ValueError("vertex already exists:" + name)
 
 	def add_adge(self,name_source,name_target):
-		source = find_vertex(name_source)
-		source.add_adjecency(name_target)
+		source = self.find_vertex(name_source)
+		if(source is None):
+			raise ValueError("no such vertex:" + name_source)
+		target = self.find_vertex(name_target)
+		if(target is None):
+			raise ValueError("no such vertex:" + name_target)
+		if(name_source == name_target):
+			raise ValueError("cant connect vertex to itself:" + name_source)
+		if not self.is_cycle(source,target):
+			source.add_adjecency(target)
+			target.parents.add(source.parents)
+			target.parents.add(source)
+		else:
+			raise ValueError("cycle is not allowed:" + name_target)
 				
 	def find_vertex(self,name = None):
 		for node in self.vertices:
 			if node.name == name:
 				return node
 		return None
+
+	def is_cycle(self,vertex_from,vertex_to):
+		if vertex_to in vertex_from.parents:
+			return True
+		return False
+
+
+
+class TopologicalSort:
+	def __init__(self):
+		self.dr = DirectedGraph()
