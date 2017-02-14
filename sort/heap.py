0f3488d28506cdfeda1d3112a6ee81ca86c7b0e4
Yehuda Sapir <yehuda.sapir@Yehudas-MacBook-Pro.local>
2017-02-14 21:53:02 +0200

    add heap basic functionalities and unit testing start

--- sort/heap.py
+++ sort/heap.py
@@ -1,41 +1,153 @@
 import sys
 
 class HeapNode:
-	def __init__(self, value = None, cargo = None, parent = None):
+	def __init__(self, value = None, cargo = None):
 		self.value = value
 		self.cargo = cargo
-		self.parent = parent
+	
+	def __eq__(self,other):
+		return self.value==other
 
-class heap:
+	def __str__(self):
+		return str(self.value)
+
+	__repr__ = __str__
+
+class Heap:
 	def __init__(self, is_min = True):
 		self.heap_arr = [sys.maxint]
 		self.is_min = is_min
+
+	def __str__(self):
+		return str(self.heap_arr[1:])
+
+	__repr__ = __str__
 
 	def add(self, node = None):
 		if node == None:
 			raise ValueError("cant add None value to heap!")
 		self.heap_arr.append(node)
-		size = len(self.heap_arr)
-		if size > 2:
-			node.parent = self.heap_arr[size/2]
-		else:
-			node.parent = None
-		bubble_up(node)
+		index = len(self.heap_arr) - 1
+		self.bubble_up(index/2,index)
 
-	def bubble_up(self,node):
-		if(node == None):
+	def delete(self,obj=None):
+		if(obj==None):
 			return
-		if(node.parent ==  None):
+		if isinstance(obj,int):
+			self.delete_by_index(obj)
+		elif(isinstance,HeapNode):
+			self.delete_by_node(obj)
+
+	def delete_by_node(self,obj):
+		index = self.find_node(obj)
+		if(index>0):
+			self.delete_by_index(index)
+
+	def find_node(self,obj):
+		if(obj==None):
 			return
-		if(node.value < node.parent.value):
+		for index,node in enumerate(self.heap_arr):
+			if(obj==node):
+				return index
+		return -1
+
+	def delete_by_index(self,index):
+		lenth=len(self.heap_arr)
+		if(index>(lenth-1)):
+			return
+		last=self.heap_arr.pop()
+		self.heap_arr[index]=last
+		self.bubble_down(index,index*2)
+		self.bubble_down(index,index*2+1)
+
+	def pop(self):
+		head=self.heap_arr[1]
+		last = self.heap_arr.pop()
+		if(len(self.heap_arr)>1):
+			self.heap_arr[1]=last
+			self.bubble_down(1,2)
+			self.bubble_down(1,3)
+		return head
+
+	def bubble_up(self,parent_index, child_index):
+		lenth = len(self.heap_arr)
+		if (parent_index > (lenth-1) or child_index > (lenth-1)):
+			return
+		if(parent_index == 0 or child_index == 0):
+			return
+		if(self.heap_arr[parent_index].value < self.heap_arr[child_index].value):
 			if(self.is_min):
 				return
 			else:
-				self.swap(node,node.parent)
-				self.bubble_up(node)
+				self.swap(parent_index,child_index)
+				self.bubble_up(parent_index/2,parent_index)
+				return
 		else:
 			if(self.is_min):
-				self.swap(node,node.parent)
-				self.bubble_up(node)
+				self.swap(parent_index,child_index)
+				self.bubble_up(parent_index/2,parent_index)
+				return
 			else:
 				return
+
+	def bubble_down(self,parent_index, child_index):
+		lenth = len(self.heap_arr)
+		if (parent_index > (lenth-1) or child_index > (lenth-1)):
+			return
+		if(self.heap_arr[parent_index].value < self.heap_arr[child_index].value):
+			if(self.is_min):
+				return
+			else:
+				self.swap(parent_index,child_index)
+				self.bubble_down(child_index,child_index*2)
+				self.bubble_down(child_index,child_index*2+1)
+				return
+		else:
+			if(self.is_min):
+				self.swap(parent_index,child_index)
+				self.bubble_down(child_index,child_index*2)
+				self.bubble_down(child_index,child_index*2+1)
+				return
+			else:
+				return
+
+	def swap(self,source_index,target_index):
+		tmp = self.heap_arr[source_index]
+		self.heap_arr[source_index] = self.heap_arr[target_index]
+		self.heap_arr[target_index] = tmp
+
+
+if __name__ == "__main__":
+	heap = Heap()
+	heap.add(HeapNode(value = 5))
+	heap.add(HeapNode(value = 9))
+	heap.add(HeapNode(value = 3))
+	heap.add(HeapNode(value = 7))
+	heap.add(HeapNode(value = 11))
+	heap.add(HeapNode(value = 1))
+	print heap
+	print heap.pop()
+	print heap
+	heap.delete(2)
+	print heap
+	while(len(heap.heap_arr)>1):
+		print heap.pop(),",",
+	print "\n"
+
+	print "-----------------------"
+
+	heap = Heap(is_min = False)
+	heap.add(HeapNode(value = 5))
+	heap.add(HeapNode(value = 9))
+	heap.add(HeapNode(value = 3))
+	heap.add(HeapNode(value = 7))
+	heap.add(HeapNode(value = 11))
+	heap.add(HeapNode(value = 1))
+	print heap
+	print heap.pop()
+	print heap
+	heap.delete(2)
+	print heap
+	while(len(heap.heap_arr)>1):
+		print heap.pop(),",",
+	print "\n"
--- sort/test_sort.py
+++ sort/test_sort.py
@@ -35,3 +35,65 @@ class TestBinaryTree:
 			my_tree.insert(my_arr[i])
 		assert my_tree.traverse_dfs_tree(my_tree.head) == [2,4,5,7,13,15,19,20,22]
 
+import heap
+class TestHeap:
+	def test_add_min(self):
+		hp = heap.Heap()
+		hp.add(heap.HeapNode(value = 5))
+		assert hp.heap_arr[1]==5
+		hp.add(heap.HeapNode(value = 9))
+		assert hp.heap_arr[2]==9
+		hp.add(heap.HeapNode(value = 3))
+		assert hp.heap_arr[1]==3
+
+	def test_add_max(self):
+		hp = heap.Heap(is_min=False)
+		hp.add(heap.HeapNode(value=5))
+		assert hp.heap_arr[1]==5
+		hp.add(heap.HeapNode(value=9))
+		assert hp.heap_arr[1]==9
+		hp.add(heap.HeapNode(value=3))
+		assert hp.heap_arr[3]==3
+
+	def test_delete_max(self):
+		hp = heap.Heap(is_min=False)
+		hp.add(heap.HeapNode(value=5))
+		hp.add(heap.HeapNode(value=9))
+		hp.add(heap.HeapNode(value=3))
+		hp.delete(10)
+		hp.delete(1)
+		assert hp.heap_arr[1]==5
+		hp.delete(heap.HeapNode(value=5))
+		assert hp.heap_arr[1]==3
+
+	def test_delete_min(self):
+		hp = heap.Heap(is_min=True)
+		hp.add(heap.HeapNode(value=5))
+		hp.add(heap.HeapNode(value=9))
+		hp.add(heap.HeapNode(value=3))
+		hp.delete(10)
+		hp.delete(1)
+		assert hp.heap_arr[1]==5
+		hp.delete(heap.HeapNode(value=5))
+		assert hp.heap_arr[1]==9	
+
+
+
+	#def test_delete_min(self):	
+
+
+# heap = Heap()
+# heap.add(HeapNode(value = 5))
+# heap.add(HeapNode(value = 9))
+# heap.add(HeapNode(value = 3))
+# heap.add(HeapNode(value = 7))
+# heap.add(HeapNode(value = 11))
+# heap.add(HeapNode(value = 1))
+# print heap
+# print heap.pop()
+# print heap
+# heap.delete(2)
+# print heap
+# while(len(heap.heap_arr)>1):
+# 	print heap.pop(),",",
+# print "\n"
